737c4f60869de64914cb2073e2f7218db2de57fd
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2017-04-08 12:37:08 +0600

    Implemented bookmarklet.

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(l){var m=/^\/cup\/(\d+)/.exec(l.pathname),d=+m[1];d?(location.href="http://dvorkin.no-ip.org/"+d):alert("Перейдите на турнир GoodGame и попробуйте снова.")})(location)
